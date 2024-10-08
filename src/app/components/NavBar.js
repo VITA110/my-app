400e9aa02ba3348cb168f92d184c6ac079781631
VITA110 <hugo.cordova709@gmail.com>
2024-10-08 10:04:21 -0600

    Primeros pasos en NEXT creación de login y algo mas no se.

---
+++ src/app/components/NavBar.js
@@ -0,0 +1,14 @@
+export default function NavBar(){
+    return(
+        <div>
+            <ul>
+                <li>
+                    <a>sss</a>
+                </li>
+                <li>
+                    <a>www</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,15 @@
+export default function Login(){
+    return (
+    <div classname="mainDivLogin">
+        <h1>Este es un login</h1>
+        <div>
+            <form>
+                s
+            </form>
+        </div>
+    </div>
+
+
+    );
+
+}
